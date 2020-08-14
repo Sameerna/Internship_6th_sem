96a20f75fcd82ac913ef5559cb48a4ab4fc9d0a2
Sameerna Joshi <sameernajoshi@gmail.com>
2020-08-14 16:46:35 +0530

    Updates

---
+++ precache-manifest.b088e85036806a93e6f393e826797ae0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af4204fa51adfce1881f861a919e73bf",
+    "url": "/Internship_host/index.html"
+  },
+  {
+    "revision": "ef3ea626cac1be10443a",
+    "url": "/Internship_host/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "2acc87aa1f2bc89d321f",
+    "url": "/Internship_host/static/css/main.bdee3aa2.chunk.css"
+  },
+  {
+    "revision": "ef3ea626cac1be10443a",
+    "url": "/Internship_host/static/js/2.9daf1f33.chunk.js"
+  },
+  {
+    "revision": "2acc87aa1f2bc89d321f",
+    "url": "/Internship_host/static/js/main.c111fec1.chunk.js"
+  },
+  {
+    "revision": "fa4ce5642c3154e64d6a",
+    "url": "/Internship_host/static/js/runtime~main.401ccbf6.js"
+  }
+]);
